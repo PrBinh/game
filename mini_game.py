8c0e11ac024e6bfe179de4dd773ef2d45205ec1a
PrBinh <huybinh650@gmail.com>
2020-08-28 08:01:00 +0700

    Update mini_game.py

--- mini_game.py
+++ mini_game.py
@@ -1,5 +1,5 @@
 from random import randint
-player = input("Moi chon bao, bua hoac keo \n")
+player = input("Moi chon bao, bua hoac keo" \n)
 computer = randint(0,2)
 if computer == 0:
 	computer = "bao"
